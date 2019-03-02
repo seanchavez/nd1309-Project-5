37b5ef88f9a18a797534bcc0b23f0fbee6834521
Sean Chavez <realseanchavez@gmail.com>
2019-03-02 15:15:49 -0800

    Implement lookup method

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,5 @@
-import Web3 from "web3";
-import starNotaryArtifact from "../../build/contracts/StarNotary.json";
+import Web3 from 'web3';
+import starNotaryArtifact from '../../build/contracts/StarNotary.json';
 
 const App = {
   web3: null,
@@ -22,41 +22,48 @@ const App = {
       const accounts = await web3.eth.getAccounts();
       this.account = accounts[0];
     } catch (error) {
-      console.error("Could not connect to contract or chain.");
+      console.error('Could not connect to contract or chain.');
     }
   },
 
   setStatus: function(message) {
-    const status = document.getElementById("status");
+    const status = document.getElementById('status');
     status.innerHTML = message;
   },
 
   createStar: async function() {
     const { createStar } = this.meta.methods;
-    const name = document.getElementById("starName").value;
-    const id = document.getElementById("starId").value;
-    await createStar(name, id).send({from: this.account});
-    App.setStatus("New Star Owner is " + this.account + ".");
+    const name = document.getElementById('starName').value;
+    const id = document.getElementById('starId').value;
+    await createStar(name, id).send({ from: this.account });
+    App.setStatus('New Star Owner is ' + this.account + '.');
   },
 
   // Implement Task 4 Modify the front end of the DAPP
-  lookUp: async function (){
-    
-  }
-
+  lookUp: async function() {
+    const { lookUptokenIdToStarInfo } = this.meta.methods;
+    const id = document.getElementById('lookid').value;
+    const starName = await lookUptokenIdToStarInfo(id);
+    console.log('STAR_NAME: ', starName);
+    App.setStatus(`Star #${id} is called ${starName}.`);
+  },
 };
 
 window.App = App;
 
-window.addEventListener("load", async function() {
+window.addEventListener('load', async function() {
   if (window.ethereum) {
     // use MetaMask's provider
     App.web3 = new Web3(window.ethereum);
     await window.ethereum.enable(); // get permission to access accounts
   } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
+    console.warn(
+      'No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live',
+    );
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
+    App.web3 = new Web3(
+      new Web3.providers.HttpProvider('http://127.0.0.1:9545'),
+    );
   }
 
   App.start();
